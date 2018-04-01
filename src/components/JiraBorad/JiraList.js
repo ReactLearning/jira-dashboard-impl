c34335a2e791f063ff9ea69d73886010c8795966
Parth Patel <parth.patel@amd.ibaset.com>
2018-04-01 17:18:13 +0530

    Display jira in table

--- src/components/JiraBorad/JiraList.js
+++ src/components/JiraBorad/JiraList.js
@@ -1,41 +1,60 @@
 import React from 'react';
 import ReactTable from 'react-table';
 import 'react-table/react-table.css';
+import '../JiraBorad/JiraList.css';
 
 const JiraList = (props) => {
-    let jiraIssues = props.issues;
-    let jiraList = "";
-    console.log(jiraIssues);
-    const columns = [{
-        Header: 'Jira ID',
-        accessor: 'key' // String-based value accessors!
-      }, {
-        Header: 'Desc',
-        accessor: 'fields.summary'
-      }, {        
-        Header: 'Priority',
-        accessor: 'fields.priority.name' // Custom value accessors!
-      },
-      {        
-        Header: 'Assignee',
-        accessor: 'fields.assignee.displayName' // Custom value accessors!
-      },
-      {        
-        Header: 'Status',
-        accessor: 'fields.status.name' // Custom value accessors!
-      } ];
-    if(jiraIssues.length >0 )
-    {
-        // jiraIssues.forEach(jira => {
-           
-        // });
-    }
-    else{
+  let jiraIssues = props.issues;
+  let jiraList = "";
+  console.log(jiraIssues);
+  const columns = [{
+    Header: 'Jira ID',
+    accessor: 'key', // String-based value accessors!
+    headerClassName: 'jiraBoradHeader'
+  }, {
+    Header: 'Desc',
+    accessor: 'fields.summary',
+    headerClassName: 'jiraBoradHeader'
+  }, {
+    Header: 'Priority',
+    accessor: 'fields.priority', // Custom value accessors!
+    className: 'jiraPriority',
+    headerClassName: 'jiraBoradHeader',
+    Cell: props => <span><img src={props.value.iconUrl} title={props.value.name} /> {props.value.name}</span>
+  },
+  {
+    Header: 'Assignee',
+    accessor: 'fields.assignee.displayName', // Custom value accessors!
+    Cell: props => <span> {props.value.split(" ")[0]}</span>,
+    headerClassName: 'jiraBoradHeader'
+  },
+  {
+    Header: 'Status',
+    accessor: 'fields.status.name', // Custom value accessors!
+    headerClassName: 'jiraBoradHeader'
+  },
+  {
+    Header: 'Charge Code',
+    accessor: 'fields.customfield_10570', // Custom value accessors!
+    headerClassName: 'jiraBoradHeader'
+  }];
+  if (jiraIssues.length > 0) {
+    // jiraIssues.forEach(jira => {
 
-    }
-    return (
-        <ReactTable data={jiraIssues} columns={columns} />
-    );
+    // });
+  }
+  else {
+
+  }
+  return (
+    <ReactTable data={jiraIssues} columns={columns}
+      sortable={true}
+      showPageJump={true} multiSort={true}
+      resizable={true}
+      filterable={false}
+      className='jiraBorad'
+    />
+  );
 };
 
 export default JiraList;    
